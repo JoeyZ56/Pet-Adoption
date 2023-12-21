805cbabec8774d58574aae309b26597f9c546334
username <Jzazzi56@gmail.comn>
2023-12-20 16:49:36 -0800

    react basics refresher

---
+++ src/pet.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/prop-types */
+const Pet = (props) => {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h2>{props.animal}</h2>
+      <h2>{props.breed}</h2>
+    </div>
+  );
+};
+
+export default Pet;
