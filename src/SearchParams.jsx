dcf0cfe3ed0bb72848f781194bd8e02d3525db61
username <Jzazzi56@gmail.comn>
2023-12-20 17:01:56 -0800

    added an array of animal options

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,12 +1,28 @@
 import { useState } from "react";
 
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
 const SearchParams = () => {
   const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
 
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">Location</label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => setAnimal(e.target.value)}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option key={animal}>{animal}</option>
+            ))}
+          </select>
+        </label>
         <input
           onChange={(e) => setLocation(e.target.value)}
           id="location"
