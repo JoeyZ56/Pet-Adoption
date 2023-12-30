e8a9cfd222b8ccb362c170a3c1937bcf0e7cac64
username <Jzazzi56@gmail.comn>
2023-12-29 17:24:29 -0800

    added error boundary for details.jsx

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,6 +1,9 @@
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import fetchPet from "./fetchPet";
+import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
+import { Link } from "react-router-dom";
 
 const Details = () => {
   const { id } = useParams();
@@ -18,6 +21,7 @@ const Details = () => {
 
   return (
     <div className="details">
+      <Carousel images={pet.images} />
       <div>
         <h1>{pet.name}</h1>
         <h2>
@@ -30,7 +34,23 @@ const Details = () => {
   );
 };
 
-export default Details;
+//how to use ErrorBoundary
+function DetailsErrorBoundary(props) {
+  return (
+    <ErrorBoundary
+      errorComponent={
+        <h2>
+          This listing has an error. <Link to="/">Click here</Link> to go back
+          to the home page.
+        </h2>
+      }
+    >
+      <Details {...props} />
+    </ErrorBoundary>
+  );
+}
+
+export default DetailsErrorBoundary;
 
 //useParams() is a hook that returns an object of key/value pairs of URL parameters.
 //comes from the BroswerRouter component
