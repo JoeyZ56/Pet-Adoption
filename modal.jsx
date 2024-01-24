a4820f9d0ba3469ac045cccec078a40b1722d630
username <Jzazzi56@gmail.comn>
2024-01-23 16:59:43 -0800

    added notes to modal component

--- modal.jsx
+++ modal.jsx
@@ -4,6 +4,9 @@ import { createPortal } from "react-dom";
 const Modal = ({ children }) => {
   const elRef = useRef(null);
 
+  //If the current value of the ref (elRef.current) is null
+  //(meaning it hasn't been set yet), it creates a new div element
+  //and assigns it to elRef.current.
   if (!elRef.current) {
     elRef.current = document.createElement("div");
   }
@@ -15,7 +18,23 @@ const Modal = ({ children }) => {
     return () => modalRoot.removeChild(elRef.current);
   }, []);
 
+  //Uses createPortal to render the children of the Modal component
+  //into the DOM element referenced by elRef.current.
+  //This allows the modal content to be rendered outside the normal hierarchy
+  //of the parent components.
   return createPortal(<div>{children}</div>, elRef.current);
 };
 
 export default Modal;
+
+/*
+1. useRef is another React hook used to create mutable object that persists across renders.
+
+2. createPortal is a method from react-dom used for
+ rendering children into a DOM node that exists outside
+the DOM hierarchy of the parent component.
+
+
+
+
+*/
